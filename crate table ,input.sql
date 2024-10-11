65eca70b363265989ce3647ce86428252e426fc9
fikri 1929 <muhammad.fikri.200401@gmail.com>
2024-10-11 19:50:11 +0700

    update

--- crate table ,input.sql
+++ crate table ,input.sql
@@ -24,7 +24,7 @@ INSERT INTO T_211011700352 (Employee_ID, First_Name, Last_Name, Email, Phone_Num
 VALUES (1005, 'Rizky', 'Febian', 'Rizky@gmail.com', '081277766604', TO_DATE('08/23/2019', 'MM/DD/YYYY'), 'Satpam', 6500000.59);
 
 INSERT INTO T_211011700352 (Employee_ID, First_Name, Last_Name, Email, Phone_Number, Hire_Date, Job_Name, Salary)
-VALUES (1006, 'Anya', 'Geraldien', 'ANYA@gmail.com', '081277766605', TO_DATE('08/24/2019', 'MM/DD/YYYY'), 'Administrator', 10500000.60);
+VALUES (1006, 'Anya', 'Geraldien', 'ANYA@gmail.com', '081277766605', TO_DATE('08/24/2019', 'MM/DD/YYYY'), 'Database Administrator', 10500000.60);
 
 INSERT INTO T_211011700352 (Employee_ID, First_Name, Last_Name, Email, Phone_Number, Hire_Date, Job_Name, Salary)
 VALUES (1007, 'Ahmad', 'Akbar', 'Ahmad@gmail.com', '081277766606', TO_DATE('08/25/2019', 'MM/DD/YYYY'), 'Business Analyst', 9500000.60);
@@ -42,6 +42,8 @@ INSERT INTO T_211011700352 (Employee_ID, First_Name, Last_Name, Email, Phone_Num
 VALUES (211011700352, 'muhammad', 'Fikri', 'muhammad.fikri.200401@gmail.com', '08978223198', TO_DATE('10/07/2024', 'MM/DD/YYYY'), 'Mahasiswa', 4500000.60);
 
 
+-----------------------NO1
+
 SELECT * FROM T_211011700352;
 
 UPDATE T_211011700352
@@ -53,3 +55,57 @@ SELECT
     INITCAP(First_Name) || ' ' || LOWER(Last_Name) || ' adalah seorang ' || UPPER(Job_Name) AS "IT TEAM"
 FROM 
     T_211011700352;
+    
+    UPDATE T_211011700352
+SET Job_Name = 'Database Administrator'
+WHERE Employee_ID = 1006;
+
+SELECT Employee_ID, 
+       ROUND(MONTHS_BETWEEN(SYSDATE, Hire_Date)) AS "Sudah Bekerja Selama (Bulan)"
+FROM T_211011700352
+
+---------------------------------------------NO.2
+
+SELECT Employee_ID, ------menghitung bonus
+       First_Name,
+       TO_CHAR(Salary, 'FM999G999G999D00') AS Salary,
+       CASE 
+           WHEN Salary > 5000000 THEN 
+               CASE 
+                   WHEN Salary BETWEEN 5000000 AND 6000000 THEN 200000
+                   WHEN Salary BETWEEN 6000001 AND 7000000 THEN 100000
+                   WHEN Salary BETWEEN 7000001 AND 8000000 THEN 300000
+                   WHEN Salary BETWEEN 9000001 AND 10000000 THEN 200000
+                   WHEN Salary > 10000000 THEN 400000
+                   ELSE 0 
+               END
+           ELSE 0 
+       END AS Bonus
+FROM T_211011700352
+
+SELECT Employee_ID, First_Name, ------------------ menampilkan data sesuai 
+       'Rp' || TO_CHAR(Salary, '9G999G999D999') AS Salary, 
+       TO_CHAR(Bonus, '9G999G999D999') AS Bonus
+FROM T_211011700352
+WHERE Employee_ID BETWEEN 1002 AND 1010;
+
+SELECT First_Name,  ------output nama nim salry
+       Job_Name, 
+       TO_CHAR(Bonus, '9G999G999D999') AS Bonus
+FROM T_211011700352
+WHERE Employee_ID BETWEEN 1002 AND 1010;
+
+
+SELECT Employee_ID,   ---- tampilkan salry dari id
+       TO_CHAR(Salary, 'FM999G999G999D00') AS Salary
+FROM T_211011700352;
+
+
+SELECT Employee_ID,   --------berikan bonus kepada karyawan yang gajinya di ats 5 juta
+       ROUND(MONTHS_BETWEEN(SYSDATE, Hire_Date)) AS "Sudah Bekerja Selama (Bulan)", 
+       TO_CHAR(Salary, 'FM999G999G999D00') AS Salary,
+       CASE 
+           WHEN Salary > 5000000 THEN Salary * 0.1 
+           ELSE 0 
+       END AS Bonus
+FROM T_211011700352;
